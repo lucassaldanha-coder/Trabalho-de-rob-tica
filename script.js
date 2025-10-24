3c203768aecc4dca8f00c1d25ad10bab02618ac6
lucassaldanha-coder <lucas.saldanha@souobjetivodf.com.br>
2025-10-24 14:12:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 document.getElementById("Btn1").addEventListener("click", function() {
-  document.body.style.backgroundColor = "blue";
+  document.body.style.backgroundColor = "white";
+  document.getElementById("Div").style.backgroundColor = "white";
+  
+ let elements = document.getElementsByClassName("Text");
+for (let i = 0; i < elements.length; i++) {
+    elements[i].style.color = "black";  
+}
 
 });
 
 document.getElementById("Btn2").addEventListener("click", function() {
-  document.body.style.backgroundColor = "yellow";
+  document.body.style.backgroundColor = "black";
+   document.getElementById("Div").style.backgroundColor = "black";
+  
+let elements = document.getElementsByClassName("Text");
+for (let i = 0; i < elements.length; i++) {
+    elements[i].style.color = "rgb(17, 17, 17);";
+}
 
 });
